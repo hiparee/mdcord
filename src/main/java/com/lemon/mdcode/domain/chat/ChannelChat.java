d8c2490eb3f0eeda7ec0fa8002ee25b7082e72b5
ifi999 <kkwkjm4231@gmail.com>
2022-12-16 11:06:41 +0900

    added BaseEntity

--- src/main/java/com/lemon/mdcode/domain/chat/ChannelChat.java
+++ src/main/java/com/lemon/mdcode/domain/chat/ChannelChat.java
@@ -1,5 +1,8 @@
 package com.lemon.mdcode.domain.chat;
 
+import com.lemon.mdcode.domain.BaseEntity;
+import com.lemon.mdcode.domain.channel.ChannelList;
+import com.lemon.mdcode.domain.member.Member;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,23 +11,27 @@ import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
 @Table(name = "channel_chat")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ChannelChat {
+public class ChannelChat extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "channel_chat_id")
-    private Integer id;
+    private Long id;
 
-    @Column(name = "channel_id")
-    private Integer channelId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "channel_id")
+    private ChannelList channelList;
 
-    @Column(name = "member_id")
-    private String memberId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
     @Column(name = "content")
     private String content;
@@ -32,18 +39,14 @@ public class ChannelChat {
     @Column(name = "file_yn")
     private String fileYn;
 
+    @OneToMany(mappedBy = "channelChat")
+    private List<AttachFile> attachFiles = new ArrayList<>();
+
     @Column(name = "fix_yn")
     private String fixYn;
 
     @Column(name = "reply_chat_id")
     private String replyChatId;
-
-    @CreatedDate
-    @Column(name = "create_date")
-    private LocalDateTime createDate;
-
-    @Column(name = "create_by")
-    private String createBy;
 
     @LastModifiedDate
     @Column(name = "update_date")
--- src/main/java/com/lemon/mdcode/domain/member/Member.java
+++ src/main/java/com/lemon/mdcode/domain/member/Member.java
@@ -1,6 +1,9 @@
 package com.lemon.mdcode.domain.member;
 
 import com.lemon.mdcode.common.exception.PasswordNotMachedException;
+import com.lemon.mdcode.domain.BaseEntity;
+import com.lemon.mdcode.domain.channel.ChannelMember;
+import com.lemon.mdcode.domain.chat.ChannelChat;
 import com.lemon.mdcode.dto.member.MemberPasswordEncoder;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -13,18 +16,17 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.util.Assert;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Table(name = "member")
 @Entity
 @DynamicInsert
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
+public class Member extends BaseEntity {
 
     @Id
     @Column(name = "member_id")
@@ -39,16 +41,15 @@ public class Member {
     @Column(name = "icon_file_id")
     private Integer iconFileId;
 
+    @OneToMany(mappedBy = "member")
+    private List<ChannelChat> channelChats = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member")
+    private List<ChannelMember> channelMembers = new ArrayList<>();
+
     @Column(name = "use_yn")
     @ColumnDefault("'Y'")
     private String useYn;
-
-    @CreatedDate
-    @Column(name = "create_date")
-    private LocalDateTime createDate;
-
-    @Column(name = "create_by")
-    private String createBy;
 
     @LastModifiedDate
     @Column(name = "update_date")
@@ -58,7 +59,7 @@ public class Member {
     private String updateBy;
 
     @Builder
-    public Member(String id, String name, String password, MemberPasswordEncoder passwordEncoder, LocalDateTime createDate, String createBy) {
+    public Member(String id, String name, String password, MemberPasswordEncoder passwordEncoder, String createBy) {
         Assert.state(StringUtils.isNotBlank(id), "id may not be blank");
         Assert.state(StringUtils.isNotBlank(password), "password may not be blank");
         Assert.state(passwordEncoder != null, "passwordEncoder may not be null");
@@ -66,8 +67,7 @@ public class Member {
         this.id = id;
         this.name = name;
         this.password = this.encodePassword(password, passwordEncoder);
-        this.createDate = createDate;
-        this.createBy = createBy;
+        super.setCreateBy(createBy);
     }
 
     public String encodePassword(String password, MemberPasswordEncoder passwordEncoder) {
