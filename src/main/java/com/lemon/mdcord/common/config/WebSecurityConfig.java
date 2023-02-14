fece536124326d310d41efd2c56b43bc96240449
geunwookim <geunwoo.kim@lemonhc.com>
2023-02-15 08:43:00 +0900

    permitAll test

--- src/main/java/com/lemon/mdcord/common/config/WebSecurityConfig.java
+++ src/main/java/com/lemon/mdcord/common/config/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig {
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring()
                 .antMatchers(
-                        "/**/js/**", "/**/css/**", "/static/**"
+//                        "/**/js/**", "/**/css/**", "/static/**"
                 );
     }
 
@@ -57,10 +57,10 @@ public class WebSecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
                 .authorizeRequests()
-                    .antMatchers(HttpMethod.POST, "/api/members/signin").permitAll()
-                    .antMatchers(HttpMethod.GET, "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/", "/**/js/**", "/**/css/**", "/index.html").permitAll()
-                    .anyRequest().authenticated()
-//                .antMatchers("/**").permitAll()
+//                    .antMatchers(HttpMethod.POST, "/api/members/signin").permitAll()
+//                    .antMatchers(HttpMethod.GET, "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/", "/**/js/**", "/**/css/**", "/index.html").permitAll()
+//                    .anyRequest().authenticated()
+                .antMatchers("/**").permitAll()
             .and()
                 .exceptionHandling()
                     .accessDeniedHandler(jwtAccessDeniedHandler)
