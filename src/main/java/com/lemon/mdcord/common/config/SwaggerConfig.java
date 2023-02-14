a32b59d57ecc071e4d094d35dcf7d08a99931dc4
geunwookim <geunwoo.kim@lemonhc.com>
2023-02-15 08:38:29 +0900

    js, css ignore 수정6

--- src/main/java/com/lemon/mdcord/common/config/SwaggerConfig.java
+++ src/main/java/com/lemon/mdcord/common/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         Info info = new Info()
                 .version("v1.0.0")
-                .title("MDCODE API 명세서");
+                .title("MDCORD API 명세서");
 
         String jwtSchemeName = "Authentication";
         SecurityRequirement requirement = new SecurityRequirement().addList(jwtSchemeName);
