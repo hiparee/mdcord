5ae52fc42acb8fbe205d2ee91414d19e166d5ff0
ukkidokiyo <ukkidokiyo@gmail.com>
2023-02-15 11:04:15 +0900

    불필요한 파일 삭제

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,7 +3,6 @@ import { createApp } from "vue";
 import router from "./router/routes.js";
 // import "bootstrap/dist/css/bootstrap.css";
 import "@/assets/css/index.css";
-import "@/assets/css/custom.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import App from "./App.vue";
