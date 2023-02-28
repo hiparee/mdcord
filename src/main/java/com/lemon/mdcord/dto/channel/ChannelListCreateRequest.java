44b9d4175f36bf4bf9a632f34bb2acbff585884d
geunwookim <geunwoo.kim@lemonhc.com>
2023-02-28 10:49:36 +0900

    채널 생성 request dto - dept, order 제약 조건 추가

--- src/main/java/com/lemon/mdcord/dto/channel/ChannelListCreateRequest.java
+++ src/main/java/com/lemon/mdcord/dto/channel/ChannelListCreateRequest.java
@@ -4,9 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Getter
 @NoArgsConstructor
@@ -16,16 +14,18 @@ public class ChannelListCreateRequest {
     @Size(max = 50)
     @Schema(description = "채널명", example = "TEST", maxLength = 50)
     private String name;
-
+    
     @NotNull
     @Schema(description = "상위채널 ID", example = "1")
     private Long parentId;
 
     @NotNull
+    @Min(0) @Max(2)
     @Schema(description = "채널 레벨", example = "0")
     private Integer dept;
 
     @NotNull
+    @Positive
     @Schema(description = "채널 순서", example = "0")
     private Integer channelOrder;
 
