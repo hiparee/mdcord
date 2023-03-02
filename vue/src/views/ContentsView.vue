05ee863782c62f96a7f82937f7f171df0138ba9d
huiwonseo <huiwonseo@idatabank.com>
2023-03-02 18:02:59 +0900

    스켈레톤 추가, ChatComponent 수정, drag&drop 첨부파일 퍼블

--- vue/src/views/ContentsView.vue
+++ vue/src/views/ContentsView.vue
@@ -1,20 +1,28 @@
 <template>
-  <chat-component></chat-component>
+  <chat-component :isLoading="isLoading"></chat-component>
 </template>
 
 <script setup>
 import ChatComponent from '@/components/ChatComponent.vue';
-import { onBeforeMount, watch } from 'vue';
+import { onBeforeMount, watch, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { useChannelStore } from '@/store/store.js';
-
+const isLoading = ref(true);
 const route = useRoute();
+
 const setChatInfo = () => {
+  isLoading.value = true;
+
   // Channel Title 설정
   setChatTitle();
 
   // Chant 정보 불러오기
   // --
+
+  // chatlist callback에서 실행
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 800);
 };
 
 const setChatTitle = () => {
