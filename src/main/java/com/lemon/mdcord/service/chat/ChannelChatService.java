eda900e6436f631f1e0e830e1df2fe7a5935a4ee
geunwookim <geunwoo.kim@lemonhc.com>
2023-03-09 10:26:15 +0900

    채팅 소켓 추가

--- src/main/java/com/lemon/mdcord/service/chat/ChannelChatService.java
+++ src/main/java/com/lemon/mdcord/service/chat/ChannelChatService.java
@@ -1,4 +1,10 @@
 package com.lemon.mdcord.service.chat;
 
+import com.lemon.mdcord.domain.chat.ChannelChat;
+import com.lemon.mdcord.dto.chat.ChatCreateRequest;
+
 public interface ChannelChatService {
+
+    ChannelChat createChannelChat(ChatCreateRequest request);
+
 }
