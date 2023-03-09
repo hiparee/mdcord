e21d77e22b75c0d5d4a987b9d098da94bf2e28f5
ifi999 <kkwkjm4231@gmail.com>
2023-03-09 10:26:15 +0900

    채팅 소켓 추가

--- src/main/java/com/lemon/mdcord/dto/chat/ChatCreateRequest.java
+++ src/main/java/com/lemon/mdcord/dto/chat/ChatCreateRequest.java
@@ -1,4 +1,13 @@
 package com.lemon.mdcord.dto.chat;
 
+import lombok.Getter;
+
+@Getter
 public class ChatCreateRequest {
+
+    private Long channelId;
+    private String memberId;
+    private String content;
+    private String fileYn;
+
 }
