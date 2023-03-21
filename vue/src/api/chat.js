e3a15e0c546648f3bb249fc1b9312ad5e64b8469
huiwonseo <huiwonseo@idatabank.com>
2023-03-21 16:57:57 +0900

    채팅 구현, 파일 업로드 구현 등

--- vue/src/api/chat.js
+++ vue/src/api/chat.js
@@ -1,8 +1,8 @@
-import { chat } from '@/api/index';
+import { chat } from './index';
 
 const fetchMultiFileUpload = async formData => {
   try {
-    const res = await chat.post('/api/fileUpload', formData, {
+    const res = await chat.post('/api/chat/file-upload', formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
@@ -14,4 +14,26 @@ const fetchMultiFileUpload = async formData => {
   }
 };
 
-export { fetchMultiFileUpload };
+const fetchChatlist = async channelId => {
+  try {
+    const res = await chat.get(`/api/channels/${channelId}/chat`);
+    console.log(res);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const fetchMoreChatlist = async (channelId, chatId) => {
+  try {
+    const res = await chat.get(
+      `/api/channels/${channelId}/chat?chatId=${chatId}`,
+    );
+    console.log(res);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { fetchMultiFileUpload, fetchChatlist, fetchMoreChatlist };
--- vue/src/store/modules/chat.js
+++ vue/src/store/modules/chat.js
@@ -1,26 +1,114 @@
 import { defineStore } from 'pinia';
-import { ref, computed } from 'vue';
-
-// A서버 1번채널에 새로운 채팅 데이터 추가
-// const chatData = { ...newChatData };
-// store.chatList.value.A서버[1].채팅목록.push(chatData);
+import { ref, computed, inject } from 'vue';
+import { fetchChatlist, fetchMoreChatlist } from '@/api/chat.js';
+import { timeAgo } from '@/utils/chat.js';
+import { userProfileIcon } from '@/utils/common.js';
 
 export const useChatStore = defineStore('chat', () => {
+  const dayjs = inject('dayjs');
   const chatList = ref({
-    channel: {
-      1: [],
-      2: [],
+    channels: {
+      // 1: [{ chatId: 1 }, { chatId:  }],
+      // 2: [{ chatId: 1 }],
     },
   });
+  const lastChat = ref({
+    // 10: {
+    // chatId: 99,
+    // fileYn: false
+    // }
+  });
+  /**
+   *
+   * @param {*} channelId
+   * 처음 받아오는 50개의 채팅 리스트 데이터
+   */
+  const SET_CHAT_LIST = async channelId => {
+    const { data } = await fetchChatlist(channelId);
+    const chatData = processChatData(data);
 
-  const SET_CHAT_LIST = list => {
-    list.forEach(v => {
-      chatList.value.channel[v.channelId] = v;
+    const channels = chatList.value.channels;
+    const newChannelData = Object.values(chatData).map(chatList =>
+      chatList.reverse(),
+    );
+
+    channels[channelId] = [];
+    return new Promise(resolve => {
+      setTimeout(() => {
+        console.log('데이터 적재');
+        channels[channelId] = newChannelData
+          .concat(channels[channelId] || [])
+          .reverse();
+        resolve();
+      }, 500);
     });
   };
 
+  /**
+   *
+   * @param {*} channelId
+   * scrollTop이 0인경우 호출 더보기 50개 데이터
+   */
+  const MORE_CHAT_LIST = async channelId => {
+    const channels = chatList.value.channels;
+    const lastChatId = channels[channelId][0][0].chatId;
+    const lastFlag = channels[channelId][0][0].dataLast;
+
+    if (lastFlag) return;
+
+    const { data } = await fetchMoreChatlist(channelId, lastChatId);
+    const chatData = processChatData(data);
+    console.log(chatData);
+
+    const newChannelData = Object.values(chatData)
+      .map(chatList => chatList.reverse())
+      .reverse();
+    console.log(newChannelData);
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        channels[channelId] = newChannelData.concat(channels[channelId]);
+        resolve();
+      }, 500);
+    });
+  };
+
+  const processChatData = data => {
+    const dataLast = data.last;
+    return data.content.reduce((acc, item) => {
+      const createDate = dayjs(item.createDate).format('YYYY-MM-DD HH:mm:ss');
+      const ymdhm = dayjs(item.createDate).format('YYYYMMDDHHmm');
+      const chatKey = `${item.memberId}_${ymdhm}`;
+
+      const pushData = {
+        chatId: item.chatId,
+        content: item.content,
+        memberId: item.memberId,
+        name: item.memberName,
+        fileYn: item.fileYn,
+        iconFileId: userProfileIcon(item.memberIconId),
+        createDate: createDate,
+        timeText: dayjs(createDate).format('YYYY. MM. DD A HH:mm:ss'),
+        timeAgo: timeAgo(createDate),
+        dataLast: dataLast,
+      };
+
+      if (!acc[chatKey]) {
+        acc[chatKey] = [];
+      }
+
+      acc[chatKey].push(pushData);
+      return acc;
+    }, {});
+  };
+
+  const SEND_MESSAGE = data => {};
+
+  const RECEIVE_CHAT_DATA = data => {};
   return {
     chatList,
+    lastChat,
+    MORE_CHAT_LIST,
     SET_CHAT_LIST,
   };
 });
