58e278e44d87a43b0b1ae477c8e41eea3cdede92
geunwookim <geunwoo.kim@lemonhc.com>
2023-03-28 17:50:22 +0900

    채팅 목록 - 첨부파일목록 추가

--- src/main/java/com/lemon/mdcord/dto/chat/ChatAttachFileResponse.java
+++ src/main/java/com/lemon/mdcord/dto/chat/ChatAttachFileResponse.java
@@ -1,8 +1,7 @@
 package com.lemon.mdcord.dto.chat;
 
+import com.lemon.mdcord.domain.chat.AttachFile;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
 
 @Getter
 public class ChatAttachFileResponse {
@@ -15,4 +14,13 @@ public class ChatAttachFileResponse {
     private String createDate;
     private String createBy;
 
+    public ChatAttachFileResponse(AttachFile at) {
+        this.id = at.getId();
+        this.originFileName = at.getOriginFileName();
+        this.realFileName = at.getRealFileName();
+        this.fileExt = at.getFileExt();
+        this.fileSize = at.getFileSize();
+        this.createDate = at.getCreateDate().toString();
+        this.createBy = at.getCreateBy();
+    }
 }
