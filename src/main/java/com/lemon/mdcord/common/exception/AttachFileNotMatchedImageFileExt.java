dfa7fbdbc65f6c0e07e6697501c411e22c11e046
geunwookim <geunwoo.kim@lemonhc.com>
2023-03-29 15:19:28 +0900

    serialVersionUID 누락 수정

--- src/main/java/com/lemon/mdcord/common/exception/AttachFileNotMatchedImageFileExt.java
+++ src/main/java/com/lemon/mdcord/common/exception/AttachFileNotMatchedImageFileExt.java
@@ -3,6 +3,9 @@ package com.lemon.mdcord.common.exception;
 import org.springframework.http.HttpStatus;
 
 public class AttachFileNotMatchedImageFileExt extends AbstractException {
+
+    private static final long serialVersionUID = -6117021703234376756L;
+
     @Override
     public HttpStatus getHttpStatus() {
         return HttpStatus.OK;
--- src/main/java/com/lemon/mdcord/common/exception/ChannelAlreadyDisabledException.java
+++ src/main/java/com/lemon/mdcord/common/exception/ChannelAlreadyDisabledException.java
@@ -3,6 +3,9 @@ package com.lemon.mdcord.common.exception;
 import org.springframework.http.HttpStatus;
 
 public class ChannelAlreadyDisabledException extends AbstractException {
+
+    private static final long serialVersionUID = 5426492284824228270L;
+
     @Override
     public HttpStatus getHttpStatus() {
         return HttpStatus.OK;
