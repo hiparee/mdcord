43174a4767aff17a7b33acd498a7c1853940dea6
geunwookim <geunwoo.kim@lemonhc.com>
2023-03-29 14:58:29 +0900

    이미지 파일 조회 api

--- src/main/java/com/lemon/mdcord/repository/AttachFileRepository.java
+++ src/main/java/com/lemon/mdcord/repository/AttachFileRepository.java
@@ -3,5 +3,8 @@ package com.lemon.mdcord.repository;
 import com.lemon.mdcord.domain.chat.AttachFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AttachFileRepository extends JpaRepository<AttachFile, Long> {
+    Optional<AttachFile> findByRealFileName(String fileName);
 }
---
+++ src/main/java/com/lemon/mdcord/service/chat/AttachFileService.java
@@ -0,0 +1,7 @@
+package com.lemon.mdcord.service.chat;
+
+public interface AttachFileService {
+
+    byte[] getImageFileInfo(Long channelId, String fileName);
+
+}
